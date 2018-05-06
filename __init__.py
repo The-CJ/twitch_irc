e9b3275d1536d37536b6393b11e67eb516360a17
The-CJ <christoperjaekel@googlemail.com>
2018-05-06 17:53:06 +0200

    fixed main connection loops

--- __init__.py
+++ __init__.py
@@ -1,17 +1,24 @@
-import asyncio, time
+import asyncio, time, re, discord
+
 
 class Client():
+	from .utils import send_pong, send_nick, send_pass,	req_membership,	req_commands, req_tags
+	from .commands import send_message,	join_channel, part_channel
+
+	from .message import Message
+
 	def __init__(self, token=None, nickname=None):
 
 		self.running = False
 
 		self.token = token
 		self.nickname = nickname
-		self.server = "irc.twitch.tv"
+		self.host = "irc.twitch.tv"
 		self.port = 6667
 		self.last_ping = time.time()
 
-		self.connection = None
+		self.connection_reader = None
+		self.connection_writer = None
 		self.channels = {}
 
 		self.traffic = 0
@@ -20,190 +27,85 @@ class Client():
 		self.running = False
 		self.connection.close()
 
-	#utils
-	async def send_pong(self):
-		self.connection.send(bytes("PONG :tmi.twitch.tv\r\n", 'UTF-8'))
+	def run(self, **kwargs):
 
-	async def send_nick(self):
-		self.connection.send(bytes("NICK {0}\r\n".format(self.nickname), 'UTF-8'))
+		self.token = kwargs.get('token', None)
+		self.nickname = kwargs.get('nickname', None)
 
-	async def send_pass(self):
-		self.connection.send(bytes("PASS {0}\r\n".format(self.token), 'UTF-8'))
+		if self.token == None or self.nickname == None:
+			raise AttributeError("'token' and 'nickname' must be provided")
 
-	async def req_membership(self):
-		self.connection.send(bytes("CAP REQ :twitch.tv/membership\r\n", 'UTF-8'))
+		loop = asyncio.get_event_loop()
+		loop.run_until_complete(self.start())
 
-	async def req_commands(self):
-		self.connection.send(bytes("CAP REQ :twitch.tv/commands\r\n", 'UTF-8'))
-
-	async def req_tags(self):
-		self.connection.send(bytes("CAP REQ :twitch.tv/tags\r\n", 'UTF-8'))
-
-	async def save_print_raw_data(self, data):
-		b = ""
-		for char in data:
-			if ord(char) < 256:
-				b = b + char
-
-		print(b)
-
-	#comms
-	async def send_message(self, channel, message):
-		if self.traffic <= 29:
-			self.connection.send(bytes("PRIVMSG #{0} :{1}\r\n".format(channel.lower(), message), 'UTF-8'))
-			self.traffic += 1
-			await asyncio.sleep(20)
-			self.traffic -= 1
-
-	async def join_channel(self, channel):
-		self.connection.send(bytes("JOIN #{0}\r\n".format(channel.lower()), 'UTF-8'))
-		#if not channel.lower() in [c.name for c in self.channels]:
-		#	self.channels.append(channel_class(self.BASE, channel))
-
-	async def part_channel(self, channel):
-		self.connection.send(bytes("PART #{0}\r\n".format(channel.lower()), 'UTF-8'))
-		#if channel in [c.name for c in self.channels]:
-		#	for c in self.channels:
-		#		if c.name.lower() == channel.lower():
-		#			self.channels.remove(c)
-
-	async def join_all_channel(self):
-		file = json.loads(open("_TWITCH_/_achtive_channels.json", "r").read())
-		for channel in file.get("channels", []):
-			await self.join_channel(channel)
-			await asyncio.sleep(30/50)
-
-		self.BASE.modules.Console.CYAN("INFO", "Joined: {} Twitch channels".format(str(len(file.get("channels", [])))))
-
-	#main connection
-	async def run(self):
+	async def start(self):
 		self.running = True
 		self.last_ping = time.time()
 
-		while self.running:
-
+		try:
 			#init connection
-			self.connection = socket.socket()
+			self.connection_reader, self.connection_writer = await asyncio.open_connection(host=self.host, port=self.port)
+			#start listen
+			asyncio.ensure_future( self.listen() )
 			self.last_ping = time.time()
 
-			try:
-				self.connection.connect((self.server, self.port))
-			except:
-				self.BASE.modules.Console.RED("ERROR", "Unable to connect to the Twitch IRC")
-				self.connection.close()
+		except:
+			self.connection.close()
+			self.last_ping = time.time()
+			await asyncio.sleep(10)
+
+		#login
+		await self.send_pass()
+		await self.send_nick()
+
+		#get infos
+		await self.req_membership()
+		await self.req_commands()
+		await self.req_tags()
+
+		#join main channel
+		await self.join_channel(self.nickname)
+
+		await self.send_message(self.nickname, "Jo")
+
+	async def listen(self):
+
+		#listen to twitch
+		while self.running:
+
+			payload = await self.connection_reader.readuntil()
+			payload = payload.decode('UTF-8')
+			print(payload+"\n")
+
+
+			#just to be sure
+			if payload in ["", " ", None]: continue
+
+			#response to PING
+			elif re.match(r'^PING', payload) != None:
 				self.last_ping = time.time()
-				await asyncio.sleep(10)
-				continue
+				await self.send_pong()
 
-			self.connection.settimeout(0.005)
+			#we are connected
+			elif re.match(r"^:tmi\.twitch\.tv 001.*", payload) != None:
+				asyncio.ensure_future( self.on_ready() )
 
-			#login
-			await self.send_pass()
-			await self.send_nick()
+			#on_message
+			elif re.match(r'^@.+\.tmi\.twitch\.tv PRIVMSG #.+', payload) != None:
+				asyncio.ensure_future( self.on_message( self.Message(payload) ) )
 
-			#get infos
-			await self.req_membership()
-			await self.req_commands()
-			await self.req_tags()
+			#on_member_join
+			elif re.match(r"^:tmi.+\.twitch\.tv JOIN #.+", payload) != None:
+				name = payload.split("!")[1]
+				name = name.split("@")[0]
 
-			#join main channel
-			await self.join_channel(self.nickname)
-			#asyncio.ensure_future( self.join_all_channel() ) #FIXME:
+				channel = payload.split("#")[1]
 
-			self.BASE.is_ready.twitch = True
-
-			#listen to twitch
-			while self.running:
-				raw_data_bytes = ""
-
-				disconnected = int(time.time()) - int(self.last_ping)
-				if int(disconnected) > 800:
-					#Twitch issn't pinging us, most likly means connection timeout --> Reconnect
-					self.BASE.modules.Console.RED("ERROR", "No Twitch IRC Server response")
-					self.connection.close()
-					break
-
-				try:
-					raw_data_bytes = raw_data_bytes + self.connection.recv(4096).decode('UTF-8')
-					raw_data = raw_data_bytes.splitlines()
-
-					if len(raw_data) == 0:
-						#Twitch send nothing, most likly means connection timeout --> Reconnect
-						break
-
-					for data in raw_data:
-						#Twitch can give more than one segment in a datacluster
-
-						#save, no Unicode debug print of all data
-						#await self.save_print_raw_data(data) # DEBUG:
-
-						#just to be sure
-						if data in ["", " ", None]: continue
-
-						#response to PING
-						elif re.match(r'^PING', data) != None:
-							self.last_ping = time.time()
-							await self.send_pong()
-
-						#we are connected
-						elif re.match(r"^:tmi\.twitch\.tv 001.*", data) != None:
-							self.BASE.modules.Console.GREEN("SUCCESS", "Twitch IRC Connected")
-
-						#on_message
-						elif re.match(r'^@.+\.tmi\.twitch\.tv PRIVMSG #.+', data) != None:
-							message = OBJECT_GENERATOR.Message(data)
-							asyncio.ensure_future( self.on_message(message) )
-
-						#on_member_join
-						elif re.match(r"^:tmi.+\.twitch\.tv JOIN #.+", data) != None:
-							name = data.split("!")[1]
-							name = name.split("@")[0]
-
-							channel = data.split("#")[1]
-
-							asyncio.ensure_future( self.BASE.modules._Twitch_.Base.on_member_join(self.BASE, channel, name) )
-
-						#on_member_leave
-						if "tmi.twitch.tv PART #" in data and data.startswith(":"):
-							name = data.split("!")[1]
-							name = name.split("@")[0]
-
-							channel = data.split("#")[1]
-
-							asyncio.ensure_future( self.BASE.modules._Twitch_.Base.on_member_leave(self.BASE, channel, name) )
-
-						#on other event
-						if ":tmi.twitch.tv USERNOTICE #" in data and data.startswith("@"):
-							event = re.search(r"msg-id=(.*?);", data)
-							if event != None:
-								event = event.group(1)
-
-							#Sub event
-							if event in ["resub","sub"]:
-
-								try:
-									sub_message = Get_classes_from_data.Twitch_sub(data)
-									asyncio.ensure_future( self.BASE.modules._Twitch_.Base.on_sub(self.BASE, sub_message) )
-								except:
-									self.BASE.modules.Console.RED("ERROR", "Failed to process Twitch Sub")
-									continue
-
-							#Raid event
-							if event in ["raid"]:
-								return #TODO: Make something with this
-								try:
-									raid__message = Get_classes_from_data.Twitch_sub(data)
-									asyncio.ensure_future( self.BASE.modules._Twitch_.Base.on_raid(self.BASE, raid__message) )
-								except:
-									self.BASE.modules.Console.RED("ERROR", "Failed to process Twitch Sub")
-									continue
-
-
-				except socket.timeout:
-					await asyncio.sleep(0.025)
-
-			self.BASE.is_ready.twitch = False
+				asyncio.ensure_future( self.BASE.modules._Twitch_.Base.on_member_join(self.BASE, channel, name) )
 
 	#events
+	async def on_ready(self):
+		print('Connected')
+
 	async def on_message(self, message):
 		pass
